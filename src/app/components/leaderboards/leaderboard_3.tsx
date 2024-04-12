3aeb6837d8fec08baf648ece01c02632aa1369e8
1023457Z <93607971+10234567Z@users.noreply.github.com>
2024-04-13 02:04:32 +0530

    Refurnish the UI of leaderboards

--- src/app/components/leaderboards/leaderboard_3.tsx
+++ src/app/components/leaderboards/leaderboard_3.tsx
@@ -3,31 +3,31 @@
 import { createClient } from "@/utils/supabase/client"
 import { useEffect, useState } from "react"
 
-export default function Leaderboard1(){
-    const [data , setData ] = useState<any>([])
+export default function Leaderboard1() {
+    const [data, setData] = useState<any>([])
     const supabase = createClient()
-    useEffect(()=> {
+    useEffect(() => {
         const fetchData = async () => {
-            const { data: lb3Data , error: fetchError } = await supabase.from('Leaderboard_Image_3').select('*').order('seconds' , {ascending: true})
+            const { data: lb3Data, error: fetchError } = await supabase.from('Leaderboard_Image_3').select('*').order('seconds', { ascending: true })
             setData(lb3Data)
         }
         fetchData()
         return () => setData([])
-    } , [])
+    }, [])
     return (
         <div className=" p-4">
             <h1 className="text-3xl font-bold mt-4">Zootopia</h1>
-            <table className="table-auto w-[50vw] border-collapse">
+            <table className="table-auto w-[50vw] border-collapse" style={{marginTop: "6px"}}>
                 <thead>
-                    <tr className="border-black border-solid" style={{ borderBottomWidth: '4px'}}>
+                    <tr style={{ border: "2px solid red" }}>
                         <th className="p-2">Rank</th>
                         <th className="p-2">Player</th>
                         <th className="p-2">Score</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {data.map((d:any , i:number) => (
-                        <tr key={i} className="border-black border-solid" style={{ borderBottomWidth: '0.1px'}}>
+                    {data.map((d: any, i: number) => (
+                        <tr key={i} className="border-black border-solid" style={{ borderBottomWidth: '0.1px' }}>
                             <td className="p-2">{i + 1}</td>
                             <td className="p-2">{d.name}</td>
                             <td className="p-2">{d.seconds}</td>
