59bd490df3c5801e055d12070e665d843a530bdc
10234567Z <93607971+10234567Z@users.noreply.github.com>
2024-04-12 17:27:39 +0530

    Add waldo image's name on home page

--- src/app/(overview)/page.tsx
+++ src/app/(overview)/page.tsx
@@ -56,7 +56,10 @@ export default function Home() {
         files.map((file, index) => {
           return (
             <div className="flex flex-col items-center justify-center" key={index}>
-              <Image src={URL.createObjectURL(file)} alt="Logo" width={320} height={400} className=" rounded-md shadow-sm hover:scale-110  transition-all ease-linear duration-300 " />
+              <Image src={URL.createObjectURL(file)} alt="Logo" width={320} height={400} className="mt-4 rounded-md shadow-sm hover:scale-110 w-[300px] h-[200px] transition-all ease-linear duration-300 " />
+              { index === 0 && <h3 className="text-2xl font-bold mt-4">Naval</h3>}
+              { index === 1 && <h3 className="text-2xl font-bold mt-4">Warground</h3>}
+              { index === 2 && <h3 className="text-2xl font-bold mt-4">Zootopia</h3>}
               <Link href={`waldo${index + 1}`} className="">
                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
                   Play
